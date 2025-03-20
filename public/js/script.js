86b85a5524b857029b56092615b51931847eadd3
hohenheimV <syakirlatiff@yahoo.com>
2025-03-20 17:08:21 +0800

    sweetAlert function

--- public/js/script.js
+++ public/js/script.js
@@ -3,7 +3,6 @@ function swalSuccess(message){
         title: 'Makluman',
         text: message,
         icon: 'success',
-        confirmButtonText: 'Great',
         timer: 1500,
         timerProgressBar: true,
         didOpen: () => {
@@ -20,7 +19,54 @@ function swalLoad(message){
         title: 'Makluman',
         text: message,
         icon: 'info',
-        confirmButtonText: 'Great',
+        timer: 1000,
+        timerProgressBar: true,
+        didOpen: () => {
+            Swal.showLoading();
+        },
+        willClose: () => {
+            console.log('Modal closed');
+        }
+    });
+}
+
+function swalError(message){
+    Swal.fire({
+        title: 'Makluman',
+        text: message,
+        icon: 'error',
+        timer: 1000,
+        timerProgressBar: true,
+        didOpen: () => {
+            Swal.showLoading();
+        },
+        willClose: () => {
+            console.log('Modal closed');
+        }
+    });
+}
+
+function swalWarning(message){
+    Swal.fire({
+        title: 'Makluman',
+        text: message,
+        icon: 'warning',
+        timer: 1000,
+        timerProgressBar: true,
+        didOpen: () => {
+            Swal.showLoading();
+        },
+        willClose: () => {
+            console.log('Modal closed');
+        }
+    });
+}
+
+function swalQuestion(message){
+    Swal.fire({
+        title: 'Makluman',
+        text: message,
+        icon: 'question',
         timer: 1000,
         timerProgressBar: true,
         didOpen: () => {
